de26ca36358707588987acff06dccb0c8f9a927e
André Dalcin <andre@planosassessoria.com.br>
2016-08-04 17:21:08 -0300

    atualizações

--- PortaoController.js
+++ PortaoController.js
@@ -1,15 +1,15 @@
-angular.module('portaoweb', []).controller('portaoCtrl', ['$http', '$scope', function($http, $scope) {
-	$scope.teste = 'Wiris';
+angular.module('portaoweb', []).controller('portaoCtrl', ['$http', '$scope', function ($http, $scope) {
+    $scope.nome = 'Wiris Alves';
 
-	$scope.garagem = function () {
-		$http.get('http://10.0.0.175:8081/?ligar=true').then(function() {}).catch(function(error){
-			console.log(error);
-		});
-	}
+    $scope.garagem = function () {
+        $http.get('http://10.0.0.175:8081/?ligar=true').then(function () { }).catch(function (error) {
+            console.log(error);
+        });
+    }
 
-	$scope.portao = function () {
-		$http.get('http://10.0.0.175:8081/?porta1=true').then(function() {}).catch(function(error){
-			console.log(error);
-		});
-	}
+    $scope.portao = function () {
+        $http.get('http://10.0.0.175:8081/?porta1=true').then(function () { }).catch(function (error) {
+            console.log(error);
+        });
+    }
 }]);
