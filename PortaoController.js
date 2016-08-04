d06f810198cbb2ca3c8ca2bc225c457ee53844ec
andre <andre.programmer@gmail.com>
2016-08-04 18:25:17 -0300

    loading

--- PortaoController.js
+++ PortaoController.js
@@ -1,15 +1,24 @@
 angular.module('portaoweb', []).controller('portaoCtrl', ['$http', '$scope', function ($http, $scope) {
     $scope.nome = 'Wiris Alves';
+	$scope.loading = false;
 
     $scope.garagem = function () {
-        $http.get('http://10.0.0.175:8081/?ligar=true').then(function () { }).catch(function (error) {
+	$scope.loading = true;
+        $http.get('http://10.0.0.175:8081/?ligar=true').then(function () {
+		$scope.loading = false;
+	}).catch(function (error) {
             console.log(error);
+		$scope.loading = false;
         });
     }
 
     $scope.portao = function () {
-        $http.get('http://10.0.0.175:8081/?porta1=true').then(function () { }).catch(function (error) {
+	$scope.loading = true;
+        $http.get('http://10.0.0.175:8081/?porta1=true').then(function () {
+			$scope.loading = false;
+	}).catch(function (error) {
             console.log(error);
+		$scope.loading = false;
         });
     }
 }]);
