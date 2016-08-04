82faf84601d4e2836a58b1bc71bfc3db5f945650
andre <andre.programmer@gmail.com>
2016-08-04 13:59:29 -0300

    inicial

---
+++ PortaoController.js
@@ -0,0 +1,15 @@
+angular.module('portaoweb', []).controller('portaoCtrl', ['$http', '$scope', function($http, $scope) {
+	$scope.teste = 'Wiris';
+
+	$scope.garagem = function () {
+		$http.get('http://10.0.0.175:8081/?ligar=true').then(function() {}).catch(function(error){
+			console.log(error);
+		});
+	}
+
+	$scope.portao = function () {
+		$http.get('http://10.0.0.175:8081/?porta1=true').then(function() {}).catch(function(error){
+			console.log(error);
+		});
+	}
+}]);
